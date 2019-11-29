7ded58bc1fa4e859c7a2014a9489210c5b26535a
Rafael Paredes <Rafael.X.Paredes.-ND@disney.com>
2019-11-29 17:27:23 -0600

    DB connection and dao structure, new endpoints, etc

--- src/main/java/com/acc/rest/EmployeeService.java
+++ src/main/java/com/acc/rest/EmployeeService.java
@@ -3,12 +3,19 @@ package com.acc.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.acc.bean.Employee;
+import com.acc.dao.EmployeeDao;
+import com.acc.json.EmployeeJSON;
  
 @Path("/employees")
 public class EmployeeService {
@@ -19,55 +26,53 @@ public class EmployeeService {
 	public List<Employee> getEmployees() {
 		
 		ArrayList<Employee> listEmployees = new ArrayList<Employee>();
-		Employee employee = new Employee();
-		
-		employee.setSysId(1);
-		employee.seteId("carlos.baez");
-		employee.setLevel(11);
-		employee.setLocation("CD-MX");
-		employee.setName("Baez Carlos");
-		employee.setBithday("00/00/00");
-		
-		Employee employee2 = new Employee();
-		employee2.setSysId(2);
-		employee2.seteId("s.coronado.andrad");
-		employee2.setLevel(12);
-		employee2.setLocation("MTY");
-		employee2.setName("Coronado Andrade, S.");
-		employee2.setBithday("00/00/00");
-
-		Employee employee3 = new Employee();
-		employee3.setSysId(3);
-		employee3.seteId("g.rodriguez.lozano");
-		employee3.setLevel(10);
-		employee3.setLocation("CD-MX");
-		employee3.setName("Rodriguez Lozano, G.");
-		employee3.setBithday("00/00/00");
-		
-		Employee employee4 = new Employee();
-		employee4.setSysId(4);
-		employee4.seteId("r.sanchez.rojas");
-		employee4.setLevel(10);
-		employee4.setLocation("MTY");
-		employee4.setName("Sanchez Rojas, R.");
-		employee4.setBithday("00/00/00");
-		
-		Employee employee5 = new Employee();
-		employee5.setSysId(5);
-		employee5.seteId("ivan.albor");
-		employee5.setLevel(9);
-		employee5.setLocation("USA");
-		employee5.setName("Albor, Ivan");
-		employee5.setBithday("00/00/00");
-		
-		listEmployees.add(employee);
-		listEmployees.add(employee2);
-		listEmployees.add(employee3);
-		listEmployees.add(employee4);
-		listEmployees.add(employee5);
-		
+		EmployeeDao employeDao = new EmployeeDao();
+		listEmployees = employeDao.getEmployees();
 		return listEmployees;
  
 	}
+	
+	@GET
+	@Path("/{sysId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Employee getEmployees(@PathParam("sysId") int sysId) {
+		// Refactorizar este método para que se conecte con la bd
+		// 1.-En el DAO crear un método llamado getEmployeeBySysId que busque 
+		// empleados por sysId y 
+		// 2.- Remplazar el código hardcodeado aquí por el objeto employee 
+		// que regresaría el método getEmployeeBySysId
+		Employee employee = new Employee();
+		
+		if ( sysId == 1 ) {			
+			employee.setSysId(1);
+			employee.seteId("carlos.baez");
+			employee.setLevel(11);
+			employee.setLocation("CD-MX");
+			employee.setName("Baez Carlos");
+			employee.setBithday("00/00/00");
+		} else {
+			// return an error
+			 throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		
+		return employee;
  
+	}
+ 
+	/**
+	 * Crear una API que inserte los datos de un employee
+	 * 1 = Created
+	 * 0 = Already exist
+	 * -1 = Errors
+	 */
+	
+	@POST @Consumes("application/json")
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public int insertEmployee(final EmployeeJSON employeeJson) {
+		int result = 1;
+		System.out.println("Name" + employeeJson.sysId);
+		// Insertar aquí el código para insertar los datos en la db
+		return result;
+	}
 }
---
+++ src/main/java/com/acc/util/ConnectorDerby.java
@@ -0,0 +1,29 @@
+package com.acc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectorDerby {
+	private static final ConnectorDerby connector = new ConnectorDerby();
+	
+	private final String DEB_URL = "jdbc:derby:C:\\Users\\r.paredes.bernal\\derbydb\\skills_dashboard_db;create=true";
+	private Connection conn;
+
+	private ConnectorDerby() {
+		try {
+			conn = DriverManager.getConnection(DEB_URL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static ConnectorDerby  getConnector() {
+		return connector;
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+
+}
